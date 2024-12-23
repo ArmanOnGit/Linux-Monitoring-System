5681daa6a2200fdcd48e064e819342529b663b5e
Arman <real.qundix1105@gmail.com>
2024-12-23 20:35:01 +0330

    Disk Script Added.

---
+++ scripts/Disk/wholeDisk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo " "
+function nvmeDisks()
+{
+    diskPartitions=$(lsblk | grep disk | awk '{print $1}')
+    for items in $diskPartitions; do
+        i=1
+        for item in $diskPartitions; do
+            lsblk -o TYPE,NAME,SIZE | grep disk | head -n $i
+            echo "*"
+            lsblk -o NAME,TYPE,SIZE,MOUNTPOINT,FSAVAIL | grep $item | grep part | sed 's/part//g' | sed 's/├─//g' | sed 's/└─//g' | sed 's/`-/|-/g' | sed 's/\//|-\//'
+            echo
+            i+=1
+        done
+    done
+
+}
+
+nvmeDisks
