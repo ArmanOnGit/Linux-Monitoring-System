c2c79498fe83727581c416268c96401e6be6b1ea
Arman <real.qundix1105@gmail.com>
2025-01-01 15:07:05 +0330

    Adding js scripts to wwwroot/js/site.js

--- src/LinuxMonitoring/wwwroot/js/site.js
+++ src/LinuxMonitoring/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+    function fetchSystemInfo() {
+        $.ajax({
+            url: '/Monitoring/GetSystemInfo',
+            type: 'GET',                  
+            success: function (response) {
+                if (response.success) {
+                        $('#systemInfoList').empty();
+                    response.systemInfo.forEach((info, index) => {
+                        $('#systemInfoList').append(`<li>${info}</li>`);
+                        if (index === 0) $('#systemInfoList').append('<br><br>');
+                    });
+                } else {
+                    $('#systemInfoList').html(`<li>Error: ${response.error}</li>`);
+                }
+            },
+            error: function () {
+                $('#systemInfoList').html('<li>An error occurred while fetching system info.</li>');
+            }
+        });
+    }
+
+    $('#SysInfo-refresh').click(fetchSystemInfo);
+
+    fetchSystemInfo();
+});
