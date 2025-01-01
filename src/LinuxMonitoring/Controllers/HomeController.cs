84357105cb93a204b894e1a0cd1c9b8cd402b3e2
Ata <amirata.tavakolii@gmail.com>
2025-01-01 14:38:01 +0330

    testing new featues

--- src/LinuxMonitoring/Controllers/HomeController.cs
+++ src/LinuxMonitoring/Controllers/HomeController.cs
@@ -27,9 +27,9 @@ public class HomeController : Controller
         var Network = _monitorService.RunScript("/app/scripts/Network/network_monitor.sh");
         string [] NetworkD = Network.Split('*');
         ViewData["NetworkData"] = NetworkD;
-        var Sysinfo = _monitorService.RunScript("/app/scripts/General/SystemInfo.sh");
-        string [] SysinfoD = Sysinfo.Split('&');
-        ViewData["Sysinfo"] = SysinfoD;
+        //var Sysinfo = _monitorService.RunScript("/app/scripts/General/SystemInfo.sh");
+        //string [] SysinfoD = Sysinfo.Split('&');
+        //ViewData["Sysinfo"] = SysinfoD;
         var Uptime = _monitorService.RunScript("/app/scripts/General/Uptime.sh");
         string[] SystemUptimeD = Uptime.Split("&");
         ViewData["Uptime"] = SystemUptimeD;
@@ -37,5 +37,33 @@ public class HomeController : Controller
         string [] SystemTimeD = SystemTime.Split('&');
         ViewData["SystemTime"] = SystemTimeD;
         return View();
+    }
+
+
+    [HttpGet("Monitoring/GetSystemInfo")]
+    public IActionResult GetSystemInfo()
+    {
+        try
+        {
+            // Run the script and process the output
+            var sysinfo = _monitorService.RunScript("/app/scripts/General/SystemInfo.sh");
+            string[] sysinfoDetails = sysinfo.Split('&');
+
+            // Return JSON response
+            return Json(new
+            {
+                success = true,
+                systemInfo = sysinfoDetails
+            });
+        }
+        catch (Exception ex)
+        {
+            // Handle errors
+            return Json(new
+            {
+                success = false,
+                error = ex.Message
+            });
+        }
     }
 }
